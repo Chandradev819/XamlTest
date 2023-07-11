cef97bec31f90cc22654be34fb06d4d62559d4f3
Chandradev <chandradev.prasad@dxc.com>
2023-07-11 23:16:52 +0530

    Add project files.

---
+++ Xaml/Counter/CounterControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace XamlTest.Xaml.Counter
+{
+    public partial class CounterControl : UserControl
+    {
+        readonly ViewModel _viewModel = new();
+        public CounterControl()
+        {
+            this.InitializeComponent();
+            this.DataContext = _viewModel;
+        }
+        private void ButtonIncrementCount_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.CurrentCount++;
+        }
+    }
+}
---
+++ Xaml/Counter/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XamlTest.Xaml.Counter
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public int _currentCount;
+        public int CurrentCount
+        {
+            get => _currentCount;
+            set
+            {
+                if (_currentCount == value) return;
+                _currentCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string callerName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+        }
+    }
+}
