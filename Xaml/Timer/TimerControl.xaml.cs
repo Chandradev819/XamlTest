695cbab7864e3424de08df31cf458b81e72e8bde
Chandradev <chandradev.prasad@dxc.com>
2023-07-12 13:26:34 +0530

    Code has been updated.

---
+++ Xaml/Timer/TimerControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace XamlTest.Xaml.Timer
+{
+    public partial class TimerControl : UserControl
+    {
+        private DispatcherTimer _timer;
+        private TimeSpan _elapsedTime;
+
+        public TimeSpan ElapsedTime
+        {
+            get => _elapsedTime;
+            set
+            {
+                _elapsedTime = value;
+                UpdateTimerValue();
+            }
+        }
+
+        public TimerControl()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += TimerTick;
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            ElapsedTime = ElapsedTime.Add(TimeSpan.FromSeconds(1));
+        }
+
+        private void StartTimer_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void StopTimer_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void ResetTimer_Click(object sender, RoutedEventArgs e)
+        {
+            ElapsedTime = TimeSpan.Zero;
+        }
+
+        private void UpdateTimerValue()
+        {
+            TimerValue.Text = ElapsedTime.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
---
+++ Xaml/WeatherForeCast/WeatherForecastControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace XamlTest.Xaml.WeatherForeCast
+{
+    public partial class WeatherForecastControl : UserControl
+    {
+        public WeatherForecastControl()
+        {
+            this.InitializeComponent();
+            DataContext = new WeatherForecastViewModel(); ;
+        }
+    }
+    public class WeatherForecastViewModel
+    {
+        public WeatherForecast[] Forecasts { get; set; }
+
+        public WeatherForecastViewModel()
+        {
+            Forecasts = new[]
+            {
+                new WeatherForecast
+                {
+                    Date = new DateTime(2023, 7, 13),
+                    TemperatureC = 25,
+                    Summary = "Sunny"
+                },
+                new WeatherForecast
+                {
+                    Date = new DateTime(2023, 7, 14),
+                    TemperatureC = 28,
+                    Summary = "Partly Cloudy"
+                },
+                // Add more forecast items as needed
+            };
+        }
+    }
+
+    public partial class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
